e592ff0c0f25aa5bd03d0f4797d819a74a772ce1
Ghion Worku <ghiongworku7@gmail.com>
2022-10-27 02:21:48 -0400

    Added App Files to Repo

---
+++ Fluidity/LoginViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Fluidity
+//
+//  Created by Ghion Worku on 10/27/22.
+//
+import UIKit
+//import Parse
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func onRegister(_ sender: Any) {
+//        let user = PFUser()
+//        user.username = usernameField.text
+//        user.password = passwordField.text
+//
+//        user.signUpInBackground{ (success, error) in
+//            if success {
+//                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+//            } else{
+//                print("Error: \(String(describing: error?.localizedDescription))")
+//            }
+//        }
+    }
+    
+    @IBAction func onLogin(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+//        PFUser.logInWithUsername(inBackground: username, password: password){ (user, error) in
+//            if user != nil {
+//                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+//            } else{
+//                print("Error: \(String(describing: error?.localizedDescription))")
+//            }
+//        }
+    }
+    
+
+}
+
